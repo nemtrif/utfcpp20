c59b9b195923bcf9d0732891ede68cba3a3530ef
Nemanja Trifunovic <ntrifunovic@hotmail.com>
2024-11-24 16:18:08 -0500

    Initial Doxygen support

--- include/utfcpp20.hpp
+++ include/utfcpp20.hpp
@@ -18,19 +18,77 @@
 #include <string>
 #include <string_view>
 
+/**
+ * \namespace utfcpp
+ */
 namespace utfcpp {
-    // Base for the exceptions that may be thrown from the library
+/**
+ * \brief Base class for the exceptions that may be thrown from the library.
+ * 
+ * All encoding/decoding errors will result in an exception of this type.
+ * Use `what` function to get a description.
+ */
     class exception : public ::std::exception {
     public:
+        /**
+         * \brief Returns the description string.
+         * 
+         * The null-terminated string with the description of the error that caused the exception.
+         * 
+         * \return Pointer to a C-style string with the description of the error.
+         * The memory is owned by the exception object.
+         */
         const char* what() const noexcept override;
     };
 
+    /**
+     * \brief Appends a code point to a UTF-8 string.
+     * 
+     * Encodes the code pont as UTF-8 and appends it to the UTF-8 string.
+     * 
+     * \param utf8string A UTF-8 encoded string to which a new code point will be appended.
+     * \param code_point Code point to encode as UTF-8 and append to the string.
+     */
     void append_to_utf8(std::u8string& utf8string, char32_t code_point);
+
+    /**
+     * \brief Appends a code point to a UTF-16 string.
+     * 
+     * Encodes the code pont as UTF-16 and appends it to the UTF-16 string.
+     * 
+     * \param utf16string A UTF-16 encoded string to which a new code point will be appended.
+     * \param code_point Code point to encode as UTF-16 and append to the string.
+     */
     void append_to_utf16(std::u16string& utf16string, char32_t code_point);
 
+    /**
+     * \brief Converts a UTF-8 encoded string to UTF-16.
+     * 
+     * Decodes a UTF-8 string and encodes its content as UTF-16.
+     * 
+     * \param utf8_string A view to a UTF-8 encoded string to convert to UTF-16.
+     * \return A UTF-16 encoded string.
+     */
     std::u16string utf8_to_16(std::u8string_view utf8_string);
+
+    /**
+     * \brief Converts a UTF-16 encoded string to UTF-8.
+     * 
+     * Decodes a UTF-16 string and encodes its content as UTF-8.
+     * 
+     * \param utf16_string A view to a UTF-8 encoded string to convert to UTF-16.
+     * \return A UTF-16 encoded string.
+     */
     std::u8string  utf16_to_8(std::u16string_view utf16_string);
 
+/// \file
+
+/**
+ * \brief Class for iterating over a UTF-8 encoded string.
+ * 
+ * The class is an input iterator that holds references to the current position and end of 
+ * the UTF-8 encoded sequence.
+ */
     class u8_iterator {
     public:
         using value_type        = char32_t;
@@ -39,11 +97,56 @@ namespace utfcpp {
         using difference_type   = std::ptrdiff_t;
         using iterator_category = std::input_iterator_tag;
     public:
+        /**
+         * \brief Returns a `u8_iterator` to the beginning of the given UTF-8 string view.
+         * 
+         * Given a `std::u8string_view` object, constructs `u8_iterator` to its beginning.
+         * 
+         * \param str_view A string view referencing a UTF-8 encoded string
+         * \return `u8_iterator` pointing to the beginning of the sequence
+         */
         static u8_iterator begin(std::u8string_view str_view);
+        /**
+         * \brief Returns a `u8_iterator` to the end of the given UTF-8 string view.
+         * 
+         * Given a `std::u8string_view` object, constructs `u8_iterator` to its end.
+         * 
+         * \param str_view A string view referencing a UTF-8 encoded string
+         * \return `u8_iterator` pointing to the end of the sequence
+         */
         static u8_iterator end(std::u8string_view str_view);
+        /**
+         * \brief Decodes the next utf-8 sequence.
+         * 
+         * Decodes the sequence the iterator is currently pointing to.
+         * 
+         * \return The code point of the current UTF-8 sequence.
+         */
         char32_t operator *() const;
+        /**
+         * \brief The three way comparison operator.
+         * 
+         * Compares two `u8_iterator` objects.
+         * 
+         * \param it An iterator to compare with.
+         * \return Zero if the two iterators are equal.
+         */
         auto operator <=>(const u8_iterator&) const = default;
+        /**
+         * \brief The prefix increment.
+         * 
+         * Increments the iterator to point to the next code point.
+         * 
+         * \return Reference to the incremented iterator.
+         */
         u8_iterator& operator ++();
+        /**
+         * \brief The postfix increment.
+         * 
+         * Increments the iterator to point to the next code point and returns the current one.
+         * 
+         * \return The original value of the iterator.
+         */
         u8_iterator  operator ++(int);
     private:
         u8_iterator(std::u8string_view::iterator begin,
